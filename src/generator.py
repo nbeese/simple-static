a9a1f7be596921b6d4721c3ed019ff34ab53765c
Nils Beese <nilsbeese1@gmail.com>
2026-09-09 14:15:07 +0200

    remove debugging statements

--- src/generator.py
+++ src/generator.py
@@ -9,22 +9,17 @@ def generate_page(from_path, template_path, dest_path):
     file = open(from_path, "r")
     md = file.read()
     file.close()
-    print(f"md: {md!r}")
 
     template_file = open(template_path, "r")
     template_content = template_file.read()
     template_file.close()
-    print(f"template_content: {template_content!r}")
 
     html_node = markdown_to_html_node(md)
     title = extract_title(md)
-    print(f"title: {title!r}")
 
     html_content = html_node.to_html()
-    print(f"html_content: {html_content!r}")
 
     updated_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
-    print(f"The contents of the html:\n {updated_content}")
 
     dir = os.path.dirname(dest_path)
     os.makedirs(dir, exist_ok=True)
