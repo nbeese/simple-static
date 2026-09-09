70a5e00577b6958907c1bc194f92deedc8d5cb14
Nils Beese <nilsbeese1@gmail.com>
2026-09-09 14:31:07 +0200

    add content, add page generation to main.py

--- src/main.py
+++ src/main.py
@@ -9,5 +9,9 @@ def main():
         shutil.rmtree("./public")
     recursive_copy("./static", "./public")
     generate_page("content/index.md","template.html","public/index.html")
+    generate_page("content/blog/glorfindel/index.md","template.html","public/blog/glorfindel/index.html")
+    generate_page("content/blog/tom/index.md","template.html","public/blog/tom/index.html")
+    generate_page("content/blog/majesty/index.md","template.html","public/blog/majesty/index.html")
+    generate_page("content/contact/index.md","template.html","public/contact/index.html")
 
 main()
